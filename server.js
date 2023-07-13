ac527e789e1f96e7cee3c62d5bed7831bc40a7c6
Trojjan <113385947+Trojjan@users.noreply.github.com>
2023-07-13 19:46:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
