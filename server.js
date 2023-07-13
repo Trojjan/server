99c9ca29d1375775f5d9f24b3dca6434f3710935
Trojjan <113385947+Trojjan@users.noreply.github.com>
2023-07-13 20:13:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/api/quotes', async (req, res) => {
   
       res.json(quote);
     } catch (error) {
-      console.error('Error fetching quotes:',  error.response.data);
+      console.error('Error fetching quotes:',  error);
       res.status(500).json({ error: 'Failed to fetch quotes' });
     }
   });
