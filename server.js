444067ab55bf8e39b365fe0d33e350a700d672b5
Trojjan <113385947+Trojjan@users.noreply.github.com>
2023-07-14 21:06:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,39 +5,50 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    process.exit(1); // Exit the process with a non-zero code to indicate failure
-  }
-}
-
-run().catch(console.dir);
-
-app.get('/api/quotes', async (req, res) => {
-  try {
-    const db = client.db("quotesgen");
-    const quotesCollection = db.collection("quotes");
-
-    const count = await quotesCollection.countDocuments();
-    const random = Math.floor(Math.random() * count);
-    const quote = await quotesCollection.findOne({}, { skip: random });
-
-    res.json(quote);
-  } catch (error) {
-    console.error('Error fetching quotes:', error);
-    res.status(500).json({ error: error });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// GET a random quote
+app.get('/api/quotes', async (req, res) => {
+    try {
+      const db = client.db("quotesgen"); 
+      const quotesCollection = db.collection("quotes"); 
+  
+      const count = await quotesCollection.countDocuments();
+      const random = Math.floor(Math.random() * count);
+      const quote = await quotesCollection.findOne({}, { skip: random });
+  
+      res.json(quote);
+    } catch (error) {
+      console.error('Error fetching quotes:',  error);
+      res.status(500).json({ error: error });
+    }
+  });
+  
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
